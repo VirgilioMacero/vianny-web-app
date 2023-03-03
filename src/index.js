d1252aa3171eb8edef85b7669f7bc78241aef68c
Virgilio Macero <virgiliomacero@gmail.com>
2023-03-03 12:03:56 -0400

    Modified all the document for begin in white

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function LogOutButton(){
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+return <button onClick={Greeteng()}>Saluda a Pablo</button>
+
+
+}
+
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+function Greeteng(){
+
+    const name = 'Pablo'
+
+    return root.render(<h1>Hola {name}</h1>)
+
+}
+
+
+root.render(<>{LogOutButton()}</>)
